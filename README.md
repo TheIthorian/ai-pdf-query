398f0956acbf2ba9b7add18d010dd6281ead34e4
TheIthorian <ben.shortland1020@gmail.com>
2023-11-26 22:32:48 +0000

    feat: initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ai-pdf-query
+
+## Install
+
+This app uses docker. To run the application:
+
+```sh
+docker compose up --build
+```
---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from src.genie import Genie
+from src.file_loader import load_pdf
+import os
+
+
+app = Flask(__name__)
+
+load_pdf("assets/owners_manual.pdf")
+genie = Genie("result.txt")
+
+
+@app.route("/")
+def hello_world():
+    return "Hello, Docker!"
+
+
+@app.route("/query/<query_input>")
+def query(query_input):
+    return "query: " + query_input
+    # return genie.ask(query_input)
